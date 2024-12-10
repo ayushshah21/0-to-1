8e6cc50344f0ea70db5855723c115aa0afce3ecf
Ayush Shah <aas225@lehigh.edu>
2024-12-10 12:16:19 -0500

    next-js intro including backend routes and server actions

---
+++ 0-100-cohort-2-live/nextjs-intro/app/actions/user.ts
@@ -0,0 +1,19 @@
+"use server"
+import client from '@/db';
+
+export async function signup(email: string, password: string) {
+    try {
+        await client.user.create({
+            data: {
+                email,
+                password
+            }
+        });
+        return true;
+    }
+    catch (e) {
+        console.log(e);
+        return false;
+
+    }
+}
